cf90dd4ed7ab1e2828b51480f50364db1d6aed7c
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-01-18 00:29:32 +0000

    Create a constant file that keeps all the constants to be used in the program:

---
+++ constants.js
@@ -0,0 +1,22 @@
+const IP = "10.0.2.15";
+const PORT = 50541;
+const MOVE_UP_KEY = "w";
+const MOVE_LEFT_KEY = "a";
+const MOVE_DOWN_KEY = "s";
+const MOVE_RIGHT_KEY = "d";
+const KEY_MAP = {
+  u: "Hi",
+  i: "GG",
+  o: "Bye",
+  p: "GJ",
+};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  KEY_MAP,
+};
